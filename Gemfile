7a6fdd271c67ad70ee23e1b488466e2f0d4944ca
Marcel Vieira <marcelfonteles@gmail.com>
2019-03-13 00:47:37 +0000

    config inicial

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'jbuilder', '~> 2.5'
 
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
+gem 'awesome_print'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root 'pages#index'
+  get 'pages/index'
+  get '/about', to: 'pages#about', as: 'about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
