a622904f311fa5caeccbc2dd0ac047c99bf95378
Marcel Vieira <marcelfonteles@gmail.com>
2019-03-15 01:53:15 +0000

    Creating View for Inserting Articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,19 @@
 class ArticlesController < ApplicationController
     def index
     end
+    def new
+        @article = Article.new
+    end
+    def create
+        # render plain: params[:article].inspect
+        @article = Article.new(articles_params)
+        @article.save
+        redirect_to root_path
+    end
+
+
+private
+   def articles_params
+       params.require(:article).permit(:title, :description)
+   end
 end
