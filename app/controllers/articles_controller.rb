3dec679cec2103c25b914ec91f4d56873e28c06e
Marcel Vieira <marcelfonteles@gmail.com>
2019-03-16 00:35:25 +0000

    controllers

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,8 +7,18 @@ class ArticlesController < ApplicationController
     def create
         # render plain: params[:article].inspect
         @article = Article.new(articles_params)
-        @article.save
-        redirect_to root_path
+        if @article.save
+            flash[:notice] = "Article was successfully created"
+            redirect_to root_path
+        else
+            render 'new'
+        end
+    end
+    def show
+        @article = Article.find(params[:id])
+    end
+    def edit
+        @article = Article.find(params[:id])
     end
 
 
